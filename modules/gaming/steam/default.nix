ffc90bad46205d898770215bad607d0051ac4a0d
fabianKoehnen <fk@ambimax.de>
2023-11-28 17:40:14 +0100

    flake update

--- modules/gaming/steam/default.nix
+++ modules/gaming/steam/default.nix
@@ -20,8 +20,22 @@
     enable = true;
     remotePlay.openFirewall = true;
   };
-
-  programs.gamescope = {
-    enable = true;
+  
+  environment.systemPackages = with pkgs; [ gamescope ];
+    nixpkgs.config.packageOverrides = pkgs: {
+    steam = pkgs.steam.override {
+      extraPkgs = pkgs: with pkgs; [
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXinerama
+        xorg.libXScrnSaver
+        libpng
+        libpulseaudio
+        libvorbis
+        stdenv.cc.cc.lib
+        libkrb5
+        keyutils
+      ];
+    };
   };
 }

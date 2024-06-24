521be13cfe2fb049ce8e6714ea3280db00a65df0
fabianKoehnen <42027473+FabianKoehnen@users.noreply.github.com>
2024-06-24 14:09:04 +0200

    prepared for tuxedosystem with bluetooth

--- hosts/work/tuxSiriusGen2/default.nix
+++ hosts/work/tuxSiriusGen2/default.nix
@@ -69,6 +69,13 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+
+  services.blueman.enable = true;
+
+  hardware.bluetooth.enable = true;
+
   # Trim SSDs periodicly
   services.fstrim.enable = true;
 

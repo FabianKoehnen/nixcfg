2afa8271cee8f0996a4754386c8bfaceb646c59b
Fabian Koehnen <fk@ambimax.de>
2024-11-26 10:14:15 +0100

    fixed merge conflicts

--- modules/desktops/cosmic/default.nix
+++ modules/desktops/cosmic/default.nix
@@ -34,41 +34,4 @@
 
   services.desktopManager.cosmic.enable = true;
   services.displayManager.cosmic-greeter.enable = true;
-  <<<<<<< HEAD
-
-    environment.systemPackages = with pkgs; [
-    # pyprland
-
-    libreoffice-fresh
-    gnome.gnome-disk-utility
-    baobab
-    # polkit-kde-agent
-    okular
-    vlc
-    libnotify
-    xarchiver
-
-    # Audio
-    easyeffects
-    pamixer
-    pavucontrol
-    helvum
-    playerctl
-
-    # # Sreenshot
-    # grimblast
-
-    # Clipboard
-    cliphist
-    wl-clipboard
-
-    # Xfce Tools
-    xfce.ristretto
-    xfce.thunar
-    xfce.xfce4-taskmanager
-    xfce.mousepad
-    xfce.exo
-  ];
-  =======
-  >>>>>>> main
-    }
+}

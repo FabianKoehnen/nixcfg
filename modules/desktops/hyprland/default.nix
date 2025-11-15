9042f322f9d03aae318da996438a6b1fa4c6c9fd
FabianKoehnen <42027473+FabianKoehnen@users.noreply.github.com>
2025-11-15 16:28:26 +0100

    cleanup

--- modules/desktops/hyprland/default.nix
+++ modules/desktops/hyprland/default.nix
@@ -170,6 +170,15 @@
           # "".text = ''
           # '';
         };
+        sessionVariables = {
+          GDK_BACKEND = "wayland,x11";
+          QT_QPA_PLATFORM = "wayland;xcb";
+          CLUTTER_BACKEND = "wayland";
+          XDG_CURRENT_DESKTOP = "Hyprland";
+          XDG_SESSION_TYPE = "wayland";
+          XDG_SESSION_DESKTOP = "Hyprland";
+          WLR_NO_HARDWARE_CURSORS = "1";
+        };
         pointerCursor = {
           # package = pkgs.phinger-cursors;
           # name = "phinger-cursors-light";
